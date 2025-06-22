48c97071f6c69747e65bd61e13582afa682751ef
poorvi-v-mallapur <purvigowda88@gmail.com>
2025-06-22 13:41:44 +0530

    Integrated with MySQL

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect
+from db.connect import get_connection
 
 app = Flask(__name__)
 
-# Home Route
+# Home Route - Displays cars
 @app.route('/')
-def home():
+def index():
     car_names = [
         "Innova Crysta",
         "Tempo Traveller",
@@ -17,8 +18,23 @@ def home():
     ]
     return render_template('index.html', car_names=car_names)
 
+# Form Submission Route
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
 
-# Thank You Page (for later after contact form)
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO contact_messages (name, email, message) VALUES (%s, %s, %s)", (name, email, message))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return redirect('/thankyou')
+
+# Thank You Page
 @app.route('/thankyou')
 def thankyou():
     return render_template('thankyou.html')
